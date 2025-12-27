a964ca16c4782ab4ff378f340b088ad436deaa18
Michal Cohen <m416370@gmail.com>
2025-12-27 23:13:25 +0200

    Delete adkComponentRegistry.ts file

--- frontend/src/adkComponentRegistry.ts
+++
@@ -1,5 +0,0 @@
-import AnomalyVisualizationDashboard from "./components/AnomalyVisualizationDashboard";
-
-export const componentRegistry: Record<string, React.ComponentType<any>> = {
-  AnomalyVisualizationDashboard,
-};
